af96eb27832e68eee184981b87d5ec6743d1f447
Adam Hardoumi <adam.hardoumi@gmail.com>
2021-06-16 11:56:47 +0200

    Update AsyncExecutor.java
    typo

--- src/main/java/com/beckman/offers/config/AsyncExecutor.java
+++ src/main/java/com/beckman/offers/config/AsyncExecutor.java
@@ -22,7 +22,7 @@ public class AsyncExecutor {
         executor.setCorePoolSize(2);
         executor.setMaxPoolSize(2);
         executor.setQueueCapacity(100);
-        executor.setThreadNamePrefix("CarThread-");
+        executor.setThreadNamePrefix("OffersThread-");
         executor.initialize();
         return executor;
     }
